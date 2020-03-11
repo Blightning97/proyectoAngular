5d6b3840d06ffa0781f3528d12a4471579b56ac9
Jason <jasonzatre@hotmail.com>
2020-03-11 13:50:17 -0600

    Cambios de prueba y creación de componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TituloComponent } from './titulo/titulo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent
   ],
   imports: [
     BrowserModule,
